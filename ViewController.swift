ffb7d6d30636a2aae7cfb1dd194f4e365d623b05
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-02-07 16:51:09 +1300

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Mel John del Barrio on 7/02/17.
+//  Copyright © 2017 Mel John del Barrio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
